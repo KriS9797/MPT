10724ded7cf1711ee6f6cb1cefcb5ce303b4ab74
DamianPiasecki <56433052+DamianPiasecki@users.noreply.github.com>
2021-01-09 18:55:26 +0100

    Update syn_flood.py

--- syn_flood.py
+++ syn_flood.py
@@ -1,12 +1,3 @@
-# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
 # implied.
@@ -76,6 +67,8 @@ class SimpleSwitch12(app_manager.RyuApp):
             flags=0, match=match, instructions=inst)
         datapath.send_msg(mod)
 
+
+
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         msg = ev.msg
@@ -119,8 +112,15 @@ class SimpleSwitch12(app_manager.RyuApp):
             dst_ip = pkt_ipv4[0].dst
             seq = tcp_pkt.seq
             ack = tcp_pkt.ack
+            self.logger.info(tcp_pkt.has_flags(tcp.TCP_SYN))
+            self.logger.info(tcp_pkt.has_flags(tcp.TCP_ACK))
 
             if tcp_pkt.has_flags(tcp.TCP_SYN) and not tcp_pkt.has_flags(tcp.TCP_ACK):
+                if self.in_dictlist('src_ip', src_ip, self.AttackDict):
+                    self.logger.info("TUUUTAJ")
+                    self.increase_attack_counter('src_ip', src_ip, 'attack_counter', self.AttackDict)
+                else:
+                    self.AttackDict.append({'src_ip': src_ip, 'attack_counter': 1})
                 self.SYN_Received.append({
                     'dst_ip': dst_ip,
                     'src_ip': src_ip,
@@ -129,9 +129,23 @@ class SimpleSwitch12(app_manager.RyuApp):
                     'attack': attack
                     })
 
-            elif tcp.TCP_SYN and tcp.TCP_ACK:
-                if self.in_dictlist('dst_ip', src_ip, self.SYN_Received):
+                current_counter_value = self.check_value('src_ip', src_ip, 'attack_counter', self.AttackDict)
+                self.logger.info(current_counter_value)
+                if current_counter_value == 10:
+                    self.change_value('src_ip', src_ip, 'attack', 1, self.SYN_Received)
 
+                is_attack = self.check_value('src_ip', src_ip, 'attack', self.SYN_Received)
+
+                if is_attack == 1:
+                    self.logger.info("ATTTTTTTTTTTTTTTTTTTAAAAAAAAAAAAAAAAACKKKKKKKKKKKKK")
+
+                    # < dodać wpis, żeby dropowało pakiety >
+
+            elif tcp_pkt.has_flags(tcp.TCP_SYN) and tcp_pkt.has_flags(tcp.TCP_ACK):
+                self.logger.info("1")
+                self.logger.info(self.in_dictlist('dst_ip', src_ip, self.SYN_Received))
+                if self.in_dictlist('dst_ip', src_ip, self.SYN_Received):
+                    self.logger.info("2")
                     previous_seq = self.check_value('dst_ip', src_ip, 'seq', self.SYN_Received)
                     if ack == previous_seq + 1:
                         self.SYNACK_Received.append({
@@ -141,15 +155,23 @@ class SimpleSwitch12(app_manager.RyuApp):
                         'ack': ack
                     })
 
-            elif tcp.TCP_ACK and not tcp.TCP_SYN:
+            elif not tcp_pkt.has_flags(tcp.TCP_SYN) and tcp_pkt.has_flags(tcp.TCP_ACK):
+                self.logger.info("TUTAJ")
                 if self.in_dictlist('dst_ip', src_ip, self.SYNACK_Received):
-
+                    self.logger.info("2")
                     previous_seq = self.check_value('dst_ip', src_ip, 'seq', self.SYNACK_Received)
                     previous_ack = self.check_value('dst_ip', src_ip, 'ack', self.SYNACK_Received)
                     if ack == previous_seq + 1:
                         if seq == previous_ack:
-                            if out_port != ofproto.OFPP_FLOOD:
-                                self.add_flow(datapath, in_port, dst, src, actions)
+                            self.add_flow(datapath, in_port, dst, src, actions)
+                            self.logger.info("FLOW DODANY")
+                            # pass
+                            # < dodać wpis żeby forwardowało pakiety >
+
+###############################################################
+        # install a flow to avoid packet_in next time
+        # if out_port != ofproto.OFPP_FLOOD:
+        #     self.add_flow(datapath, in_port, dst, src, actions)
 
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
